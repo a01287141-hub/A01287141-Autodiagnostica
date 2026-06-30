565b4e16017fea1f0b828980d2176bde4e9adaf2
a01287141-hub <a01287141@tec.mx>
2026-06-29 21:16:37 -0600

    Agregué métodos modificadores y de acceso

--- Complejo.cpp
+++ Complejo.cpp
@@ -3,7 +3,22 @@
 Complejo::Complejo():real{0}, imaginario{0}{
 }
 
-Complejo::Complejo(double r, double i):real{i}, imaginario{i}{
+Complejo::Complejo(double r, double i):real{r}, imaginario{i}{
 }
 
+double Complejo::getReal() const{
+  return r;
+}
+
+void Complejo::setReal(double r){
+  real = r;
+}
+
+double Complejo::getImaginario(){
+  return i;
+}
+
+void Complejo::setImaginario(double i){
+  imaginario = i;
+}
 
