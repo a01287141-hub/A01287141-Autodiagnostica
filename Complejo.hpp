36a8148a704d74ef89486e5b7cb9bb0fecc25059
a01287141-hub <a01287141@tec.mx>
2026-06-29 21:23:34 -0600

    Complejo.hpp

--- Complejo.hpp
+++ Complejo.hpp
@@ -4,16 +4,16 @@ class Complejo
 {
     public:
         Complejo();
-        Complejo(double, double);
+        Complejo(double r, double i);
         //~Complejo();
         double getReal() const;
-        void setReal(double);
+        void setReal(double r);
         double getImaginario() const;
-        void setImaginario(double);
-        void agregar(Complejo&);
+        void setImaginario(double i);
+        void agregar(Complejo& C);
         Complejo conjugado();
-        Complejo suma(Complejo&);
-        Complejo multiplicacion(Complejo&);
+        Complejo suma(Complejo& C);
+        Complejo multiplicacion(Complejo& C);
         Complejo multiEscalar(double);
         void imprime();    
     private:
